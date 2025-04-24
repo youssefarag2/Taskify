c64f52bf703167b31e283d8d752c043e70bbda63
youssefarag2 <youssefarag19@gmail.com>
2025-04-24 16:50:23 +0200

    updating routes

--- client/Taskify-client/src/App.tsx
+++ client/Taskify-client/src/App.tsx
@@ -3,13 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* <Route path="/dashboard/completed" element={<CompletedTasksPage />} /> */}
+        {/* <Route path="/settings" element={<SettingsPage />} /> */}
+      </Route>
     </Routes>
   );
 }
